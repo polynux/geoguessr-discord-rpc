265f6f8c5993106696987eab996068aa1fc9437d
Guillaume Dorce <polynux.dev@gmail.com>
2021-02-20 01:08:53 +0100

    observe value on page and send activity to the background

--- extension/content.js
+++ extension/content.js
@@ -4,25 +4,66 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   }
 });
 
+function sendStreak(streak) {
+  chrome.runtime.sendMessage({
+    game: "country-streak",
+    score: streak[0].innerText
+  });
+}
+
+function sendMap(body) {
+  chrome.runtime.sendMessage({
+    game: body[0].innerText,
+    number: body[1].innerText,
+    score: body[2].innerText
+  });
+}
+
+function observe(body, func) {
+  const observer = new MutationObserver((mutationList, observer) => {
+    for (const mutation of mutationList) {
+      if (mutation.type === "characterData") {
+        func(body.getElementsByClassName("game-status__body"));
+      }
+    }
+  });
+
+  let config = { attributes: true, childList: true, subtree: true, characterData: true };
+
+  body.childNodes.forEach(node => observer.observe(node, config));
+}
+
 function sendState() {
-  if (!window.location.href.match(/\/game\//g)) {
-    chrome.runtime.sendMessage({ message: "idle" });
-  } else {
-    chrome.runtime.sendMessage({ message: "play" });
+  let statuses = document.getElementsByClassName("game-statuses")[0];
+  let body = document.getElementsByClassName("game-statuses")[0].getElementsByClassName("game-status__body");
+  let status = document.getElementsByClassName("game-status")[0];
+
+  if (window.location.href.match(/\/game\//g)) {
+    if (status.dataset.qa === "round-number") {
+      sendStreak(body);
+      observe(statuses, sendStreak);
+    } else {
+      sendMap(body);
+      observe(statuses, sendMap);
+    }
+
+    // } else {
+    //   chrome.runtime.sendMessage({ message: "idle" });
+    // }
   }
 }
 
-window.onbeforeunload = () => {
-  chrome.runtime.sendMessage({ message: "close" });
-};
+// window.onbeforeunload = () => {
+//   chrome.runtime.sendMessage({ message: "close" });
+// };
 
 window.onfocus = () => {
   sendState();
 };
 
-window.onblur = () => {
-  chrome.runtime.sendMessage({ message: "leave" });
-};
+// window.onblur = () => {
+//   chrome.runtime.sendMessage({ message: "leave" });
+// };
 
 window.onload = () => {
   sendState();
